c7f5fce62248280e8c323f3543b705be83d81aa1
anandhuJ54 <prasadkovai@gmail.com>
2020-09-04 13:36:27 +0530

    kk

--- app/src/main/java/com/example/deafanddumbtalk/dashboard.java
+++ app/src/main/java/com/example/deafanddumbtalk/dashboard.java
@@ -3,6 +3,7 @@ package com.example.deafanddumbtalk;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +15,18 @@ public class dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        final Button text = findViewById(R.id.texttospeech);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text();
+            }
+        });
+    }
+
+    private void text() {
+        startActivity(new Intent(getApplicationContext(),TextToSpeech.class));
+        finish();
     }
 
     public void logout(View view) {
@@ -21,5 +34,7 @@ public class dashboard extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(),Login.class));
         finish();
     }
+
+
 
 }
